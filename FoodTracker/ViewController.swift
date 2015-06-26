8ce57dbb6173c36506e7d73a6a5a71336644885d
Robert McBryde <robert.mcbryde@kotikan.com>
2015-06-26 22:13:36 +0100

    Addition of text field, label and button in ViewController. Added UITextFieldDelegate conform to my ViewController and implemented necessary methods to remove keyboard and updated label withUITextField contents and click of Done button

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        // set this ViewController as the UITextFieldDelegate of my text field
+        nameTextField.delegate = self
     }
 
+    // MARK: UITextFieldDelefate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // hide the keyboard
+        nameTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default text"
+    }
 
 
 }
