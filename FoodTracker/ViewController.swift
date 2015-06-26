4a1e98fbbdd4a9ccb2084b68de2396a5db9e98f2
Robert McBryde <robert.mcbryde@kotikan.com>
2015-06-26 23:22:57 +0100

    Added imageView with tap Gesture recogniser to allow users to access UIImagePickerController() with associated delegate for cancel and imagePickerController to allow for selection from camera or photo library.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +36,40 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
     
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        // dismiss the picker 
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     // MARK: Actions
     
     @IBAction func setDefaultLabelText(sender: UIButton) {
         mealNameLabel.text = "Default text"
     }
 
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // Hide the keyboard if user tabps image while typing
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController lets us pick media from phone
+        let imagePickerController = UIImagePickerController()
+        // ensure we are notified when image is selected by being the
+        // UIImagePickerControllerDelegate delegate
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+
+    }
 
 }
 
