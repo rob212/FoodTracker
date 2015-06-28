a8cf4ef85f0fdd58ec57ecfec0bef226ac18a6ae
Robert McBryde <robert.mcbryde@kotikan.com>
2015-06-28 14:03:52 +0100

    Initial creation of RatingControl custom UIView for rating a meal.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Robert McBryde on 28/06/2015.
+//  Copyright © 2015 Robert McBryde. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    
+}
